29011bd39c8947a1d3191ca54929b990f05a8370
Anirban Madhu <koushikm203@gmail.com>
2025-11-12 00:26:25 +0530

    ok

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -3,7 +3,7 @@ dotenv.config();
 
 import app from './app';
 
-const port = Number(process.env.PORT) || 4000;
+const port = Number(process.env.PORT) || 4001;
 
 app.listen(port, () => {
   console.log(`JCB backend running on port ${port}`);
